0ddd99a620650ba4b87ec941a9b536ee3ddd6228
esther.spiteri.18@um.edu.mt <esther.spiteri.18@um.edu.mt>
2023-11-22 18:10:54 +0100

    fix

--- client/src/components/publication-search-page/publication-search-page.tsx
+++ client/src/components/publication-search-page/publication-search-page.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import styles from "./publication-search.module.scss";
+import styles from "./publication-search-page.module.scss";
 import PublicationPreview from "./publication-preview/publication-preview";
 import {
   IPublicationPreview,
