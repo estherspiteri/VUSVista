5a3fbbccae1edf655512a58d086e1efc3a2cf799
esther.spiteri.18@um.edu.mt <esther.spiteri.18@um.edu.mt>
2024-06-27 19:06:13 +0200

    proxy update

--- client/src/services/api/api.service.ts
+++ client/src/services/api/api.service.ts
@@ -1,17 +1,17 @@
 const customFetch = async (url, options) => {
   try {
     const response = await fetch(url, options);
-
+    console.log("response", response);
     if (!response.ok) {
       // Handle HTTP errors by navigating to the error page
-      window.location.href = '/error';
+      window.location.href = "/error";
     }
 
     return response.json();
   } catch (error) {
     // Handle fetch errors (e.g., network issues)
-    console.error('Fetch Error:', error);
-    window.location.href = '/error';
+    console.error("Fetch Error:", error);
+    window.location.href = "/error";
   }
 };
 
