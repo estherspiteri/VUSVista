6e87cea5df82974bd91796c2e54578f4360702bf
esther.spiteri.18@um.edu.mt <esther.spiteri.18@um.edu.mt>
2025-09-22 14:27:02 +0200

    Included example env file

--- vusvista/services/flask_server/server/config.py
+++ vusvista/services/flask_server/server/config.py
@@ -19,9 +19,9 @@ SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI')
 
 MAIL_SERVER = "smtp.gmail.com"
 MAIL_PORT = 587
-MAIL_USERNAME = 'vus.curation.system@gmail.com'
+MAIL_USERNAME = os.getenv('MAIL_USERNAME')
 MAIL_PASSWORD = os.getenv('MAIL_PASSWORD')
-MAIL_DEFAULT_SENDER = 'vus.curation.system@gmail.com'
+MAIL_DEFAULT_SENDER = os.getenv('MAIL_USERNAME')
 MAIL_USE_TLS = True
 MAIL_USE_SSL = False
 
